7d31a992eae0c9a094578eaf64ffc42104f37feb
Dominik Rutter <anastasia.v.rutter@gmail.com>
2023-11-29 16:32:06 -0500

    create basic window with event usage

--- Program.cs
+++ Program.cs
@@ -28,7 +28,50 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<RectangleShape> shapes = [
+                new RectangleShape(new Vector2f(100, 100)),
+                new RectangleShape(new Vector2f(100, 100)),
+                new RectangleShape(new Vector2f(100, 100))
+            ];
+
+            RenderWindow window = new RenderWindow(new VideoMode(1920, 1080), "Game!");
+            window.Closed += (sender, args) => { ((RenderWindow)sender).Close(); return; };
+            window.KeyPressed += (sender, args) => {
+                switch (args.Code)
+                {
+                    case Keyboard.Key.Escape:
+                        ((RenderWindow)sender).Close();
+                        break;
+                    case Keyboard.Key.Space:
+                        foreach (var shape in shapes)
+                            shape.FillColor = RandomColor();
+                        break;
+                    default: break;
+                }
+            };
+
+            
+            shapes[0].FillColor = Color.Black;
+            shapes[0].Position = new Vector2f(100, 100);
+            shapes[1].FillColor = Color.Blue;
+            shapes[1].Position = new Vector2f(200, 200);
+            shapes[2].FillColor = Color.Red;
+            shapes[2].Position = new Vector2f(300, 300);
+
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+
+                // some logic
+
+                window.Clear(Color.White);
+                foreach (var shape in shapes)
+                {
+                    window.Draw(shape);
+                }
+                window.Display();
+            }
         }
     }
 }
